e12347e03f42c88cf23f486ab366948b5456654b
Doug <drowlands02@gmail.com>
2025-05-16 23:35:25 -0600

    Fix Poetry deployment on Render

---
+++ .render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install poetry if not already present
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Add poetry to path
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install dependencies
+poetry install
