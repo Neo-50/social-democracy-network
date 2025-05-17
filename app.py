10dfe8edb9a4b9023a96790df0138477e7acd380
Doug <drowlands02@gmail.com>
2025-05-16 23:15:45 -0600

    Adding email verification with Zoho

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['MAIL_SERVER'] = 'smtp.zoho.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'admin@social-democracy.net'
-app.config['MAIL_PASSWORD'] = 'your_app_password'  # use Zoho app password here
+app.config['MAIL_PASSWORD'] = 'fluFfy4ferrEt$areNice9'  # use Zoho app password here
 app.config['MAIL_DEFAULT_SENDER'] = 'admin@social-democracy.net'
 
 mail = Mail(app)
