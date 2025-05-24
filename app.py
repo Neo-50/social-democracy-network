01cffdab2f9d6ea19024387bf50f3322a05a933e
Neo-50 <drowlands02@gmail.com>
2025-05-23 18:44:29 -0600

    More debugging

--- app.py
+++ app.py
@@ -52,6 +52,9 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
