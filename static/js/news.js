e6e9182f77d0c264f5989b7042431efd8f8fb02a
Neo-50 <drowlands02@gmail.com>
2025-07-31 20:27:32 -0600

    Reactions are being added and removed on both sides of the connection now and also added and removed from the database, next step is to make them persist after page reload

--- static/js/news.js
+++ static/js/news.js
@@ -233,7 +233,9 @@ function addUnicodeReaction(target, emoji, targetId, targetType) {
 function handleReactionClick(event) {
     const span = event.currentTarget;
     const emoji = span.dataset.emoji;
+    const targetId = span.dataset.targetId;
     const countSpan = span.querySelector(".reaction-count");
+    let action;
 
     // Parse current users
     let users = JSON.parse(span.dataset.users || "[]");
@@ -247,6 +249,7 @@ function handleReactionClick(event) {
             // Remove the reaction element entirely
             span.remove();
             console.log("Users after removal:", users);
+            action = "remove";
         } else {
             // Update count and user list
             countSpan.textContent = users.length;
@@ -257,24 +260,16 @@ function handleReactionClick(event) {
         users.push(CURRENT_USER_ID);
         countSpan.textContent = users.length;
         span.dataset.users = JSON.stringify(users);
+        action = "add";
     }
     console.log('Initiating reaction Flask route');
-    fetch("/toggle-reaction", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": window.csrfToken,
-        },
-        body: JSON.stringify({
-            target_id: span.dataset.targetId,
-            target_type: span.dataset.targetType,
-            emoji: emoji,
-            action: users.includes(CURRENT_USER_ID) ? "remove" : "add",
-        }),
+    console.log('Data payload: ', emoji, NEWS_ROOM_ID, targetId, action)
+    reactionSocket.emit("toggle_reaction", {
+        emoji,
+        target_type: NEWS_ROOM_ID,
+        target_id: targetId,
+        action,
     });
-
-
-    // TODO: Optionally: send update to server here via fetch or socket
 }
 
 document.querySelectorAll('.reply-toggle').forEach(button => {
