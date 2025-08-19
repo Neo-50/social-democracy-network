dee1611b0d5bd3610150099841959b709a31093c
Neo-50 <drowlands02@gmail.com>
2025-08-19 16:39:35 -0600

    Added more custom emojis

--- static/js/custom_emoji_list.js
+++ static/js/custom_emoji_list.js
@@ -1,17 +1,29 @@
-const customEmojis = ['clueless.png', 'pout.png', 'red_angry.png', 'gavin.png', 'kekw.png', 'kekl.png', 'lizardkek.gif', 'trolldog.png',
-    'dogesmile.png', 'pikathink.png', 'trump.png', 'guillotine.png', 'derp.png',
+const customEmojis = ['clueless.png', 'pout.png', 'red_angry.png', 'gavin.png', 'kekw.png', 'kekl.png', 'lizardkek.gif', 'leo-kek.png',
+    'trolldog.png', 'whyyy.webp', 'derp.png', 'gasp.png',
+     'trump.png', 'bernie.png', 'biden-thumbsup.png', 'noem.png', 'noem-sp.png',
+    'dogesmile.png', 'pikathink.png', 'pika-surprised.png', 'guillotine.png', 
+
     'gir.webp', 'gir-cool.webp', 'gir-hyper.webp', 'gir-stare.webp', 'gir-flat.webp', 'gir-suit.png', 'gir-happy.png', 'zimthonk.png',
-    'pepe-yes.png', 'pepe-no.png', 'hmm.png', 'peepo-ban.png', 'peepo-cute.png', 'peepo-happy.png', 'peepo-heart.png',
-    'pepe-clown.png', 'pepe-copium.png', 'pepe-cringe.png', 'pepe-cry.png', 'pepe-dumb.png', 'pepe-evil.png', 'pepe-ez.png',
-    'pepe-fu.png', 'pepe-giggle.png', 'pepe-gun.png', 'pepe-happy.png', 'pepe-holy.png', 'pepe-megacringe.png', 'pepe-nervous.png',
-    'pepe-noted.png', 'pepe-nuke.png', 'pepe-ok-boomer.png', 'pepe-omg.png', 'pepe-poggers.png', 'pepe-popcorn.png',
+    
+    'anya-this.webp', 'pika-wave.webp',  'anime-tea.webp', 'cat-coding.webp', 
+    'pepe-nod.webp', 'pepe-nope.webp', 'frog-nod.webp', 'frog-wave.webp', 'pepe-wave.webp', 'pepe-omg.webp', 'frog-ping.webp',  'kermit-ak.webp', 
+    'pepe-yes.png', 'pepe-no.png', 'hmm.png', 'peepo-ban.png', 'pepe-ban.webp', 'peepo-cute.png', 'peepo-happy.png', 'peepo-heart.png',
+    'pepe-toilet.webp', 'pepe-rain.webp', 'pepe-brero.webp', 'pepe-cringe.png', 'pepe-cringe-sign.webp', 'pepe-goose-shoot.webp',
+    'pepe-clown.png', 'pepe-copium.png', 'pepe-cry.png', 'pepe-dumb.png', 'pepe-evil.png', 'pepe-ez.png', 'pepe-kraken.webp', 'pepe-letsgo.webp',
+    'pepe-money.webp', 'pepe-mods.webp', 'pepe-party.webp', 'pepe-peek.webp', 'pepe-toxic.webp', 'pepe-sweat.webp', 'pepe-rainbow.webp',
+    'pepe-rifle.webp', 'pepe-shoot.webp',
+    'pepe-fu.png', 'pepe-giggle.webp', 'pepe-gun.png', 'pepe-happy.png', 'pepe-holy.png', 'pepe-megacringe.png', 'pepe-nervous.png',
+    'pepe-noted.webp', 'frog-noted.png', 'pepe-nuke.png', 'pepe-ok-boomer.png', 'pepe-omg.png', 'pepe-poggers.png', 'pepe-popcorn.png',
     'pepe-pray.png', 'pepe-rage.png', 'pepe-sadclown.png', 'pepe-sadge.png', 'pepe-sad-hands.png', 'pepe-salute.png', 'pepe-shades.png',
     'pepe-sip.png', 'pepe-sword.png', 'pepe-teddy-cry.png', 'pepe-think.png', 'pepe-wait.png', 'peepo-rifle.png', 'pepe-angery.png',
     'peepo-blush.png', 'peepo-sip.png', 'peepo-sit.png', 'peepo-think.png', 'pepe-angry-police.png', 'pepe-angry-scimter.png',
     'pepe-bigsmile.png', 'pepe-blankie.png', 'pepe-blubbers.png', 'pepe-chat-dead.png', 'pepe-cheer.gif', 'pepe-chef.png', 'pepe-depressed.png',
     'pepe-fightme.png', 'pepe-flower.png', 'pepe-heart02.png', 'pepe-hug.png', 'pepe-nerd.gif', 'pepe-pirate.png', 'pepe-pitchfork.png',
     'pepe-police.png', 'pepe-sleep.png', 'pepe-ukraine.png', 'pepe-wizard.png', 'pepe-yep.png', 'pepe-yikes.png',
+
+    'cat-newspaper.webp', 'cat-kiss.webp', 'cat-peek.webp', 'cat-dead.webp', 'cat-huh.webp', 'catto-huh.webp', 'cat-smug.webp', 'cat-crown.webp',
     'catblip.webp', 'catblush.webp', 'catclap.webp', 'catcool.webp', 'catcry.webp', 'catcry2.webp', 'catcry3.webp', 'catcry4.webp', 'catcry5.webp',
-    'catdance.webp', 'catjam.webp', 'catno.webp', 'catpolice.webp', 'catrave.webp', 'catsadge.png', 'catshake.webp',
-    'catstare.webp', 'catthumbsup.webp', 'cattocry.webp', 'cat_cry.webp', 'close.webp', 'clowncat.webp', 'meowyey.webp', 'nekocatsip.webp',
-    'polite.webp', 'politecri.png', 'sadcat.png', 'sadcat.webp', 'smudge.png', 'typingcat.webp', 'yellcat.webp'];
+    'catdance.webp', 'catjam.webp', 'catno.webp', 'catpolice.webp', 'catrave.webp', 'catsadge.png', 'catshake.webp', 'nyas-popcorn.webp',
+    'catstare.webp', 'cat-thumbsup.webp', 'cat-thumbsup02.webp', 'cattocry.webp', 'cat-cry.webp', 'close.webp', 'clowncat.webp', 'meowyey.webp', 'nekocatsip.webp',
+    'polite.webp', 'politecri.png', 'sadcat.png', 'sadcat.webp', 'smudge.png', 'typingcat.webp', 'yellcat.webp'
+];
